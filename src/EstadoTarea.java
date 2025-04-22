da565f04de96cf950e07b778c52d69bb1404e193
Jose David <josedavidperez4@gmail.com>
2025-04-22 16:32:12 -0600

    Movi el jdbc a lib

---
+++ src/EstadoTarea.java
@@ -0,0 +1,20 @@
+public class  EstadoTarea {
+
+        private String nombre;
+    
+        // Solo dentro de esta clase se va a determinar el estado de la tarea
+        private EstadoTarea(String nombre) {
+            this.nombre = nombre;
+        }
+    
+        @Override
+        public String toString() {
+            return nombre;
+        }
+    
+        // Estados predefinidos como constantes públicas
+        public static final EstadoTarea PENDIENTE = new EstadoTarea("Pendiente");
+        public static final EstadoTarea EN_PROGRESO = new EstadoTarea("En progreso");
+        public static final EstadoTarea COMPLETADA = new EstadoTarea("Completada");
+    
+}
---
+++ src/Observer.java
@@ -0,0 +1,4 @@
+//cada persona que quiera ser notificada tiene que inicializar el metodo actualizar
+public interface Observer {
+    void actualizar(Tarea tarea);
+}
